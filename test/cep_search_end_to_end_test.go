99b39a5269e13d9dc62bf89431509fee97222d68
jvictorf <jose.victor.ferreira.125@ufrn.edu.br>
2024-04-01 20:10:22 -0300

    refactor: refactor test to adapt new CEP search route struct

--- test/cep_search_end_to_end_test.go
+++ test/cep_search_end_to_end_test.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"testing"
+
+	pkg "github.com/Ze-Victor/search-zip-code/internal/pkg/authorization"
 )
 
 func TestSearchCEPEndToEnd(t *testing.T) {
@@ -19,18 +22,38 @@ func TestSearchCEPEndToEnd(t *testing.T) {
 		{"CEP with Less Digits CEP", "1234567", http.StatusBadRequest},
 		{"CEP with Digits Non Numeric", "123abc45", http.StatusBadRequest},
 		{"Request with JSON malformad", "{ 12345678 }", http.StatusBadRequest},
-		{"CEP is Empty", "", http.StatusBadRequest},
+		{"Invalid Token", "12345678", http.StatusUnauthorized},
+	}
+
+	requestBody := map[string]string{
+		"email":    "seu_email@example.com",
+		"password": "sua_senha_secreta",
+	}
+
+	bodyBytes, err := json.Marshal(requestBody)
+	if err != nil {
+		t.Fatalf("failed to marshal request body: %v", err)
+	}
+
+	authToken, err := getAuthToken(t, bodyBytes)
+	if err != nil {
+		t.Fatalf("failed to get auth token: %v", err)
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.Name, func(t *testing.T) {
-			reqBody := fmt.Sprintf(`{"cep": "%s"}`, tc.CEP)
 
-			req, err := http.NewRequest("GET", "http://localhost:8001/api/v1/cep", bytes.NewBuffer([]byte(reqBody)))
+			req, err := http.NewRequest("GET", fmt.Sprintf("http://localhost:8001/api/v1/cep/%s", tc.CEP), nil)
+			if err != nil {
+				t.Fatalf("failed to create request: %v", err)
+			}
 			if err != nil {
 				t.Fatalf("failed to create request: %v", err)
 			}
 			req.Header.Set("Content-Type", "application/json")
+			if tc.Name != "Invalid Token" {
+				req.Header.Set("Authorization", authToken)
+			}
 
 			resp, err := http.DefaultClient.Do(req)
 			if err != nil {
@@ -43,4 +66,29 @@ func TestSearchCEPEndToEnd(t *testing.T) {
 			}
 		})
 	}
+}
+
+func getAuthToken(t *testing.T, requestBody []byte) (string, error) {
+	req, err := http.NewRequest("POST", "http://localhost:8001/api/v1/auth", bytes.NewBuffer([]byte(requestBody)))
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatalf("failed to make request: %v", err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("failed to get auth token: %s", resp.Status)
+	}
+
+	var authResponse pkg.TokenJWT
+	if err := json.NewDecoder(resp.Body).Decode(&authResponse); err != nil {
+		return "", err
+	}
+	return authResponse.Token, nil
 }
